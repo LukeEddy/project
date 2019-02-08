2cfd4262d9cdd258301b7a3022f879775a8a4d5c
LukeEddy <42747971+LukeEddy@users.noreply.github.com>
2019-02-08 13:33:49 -0700

    added files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>();
+
+            while (true)
+
+
+
+
+
+            {
+
+
+
+                Console.WriteLine("What is the command? your options are add remove list and q as quit");
+                    
+                  
+
+
+                string command = Console.ReadLine();
+                if (command == "add")
+                {
+                    Console.WriteLine("What is the name of the game");
+
+                    // add to list 
+                    string name = Console.ReadLine();
+                    //what is the name of the game
+                    videoGames.Add(name);
+
+                    Console.WriteLine("item is now on list ");
+                    //when done say done
+
+                }
+
+
+
+                if (command == "remove")
+                {
+                    Console.WriteLine("What is the name of the game");
+
+                    // remove from list 
+                    string name = Console.ReadLine();
+                    //what is the name of the game
+                    videoGames.Remove(name);
+
+                    Console.WriteLine("item is now off list ");
+                    //when done say done
+
+                }
+
+                if (command == "list")
+                {
+
+                    videoGames.ForEach(Console.WriteLine);
+
+                }
+
+                if (command == "q")
+                    break;
+
+
+            }
+             
+        }
+
+    }
+
+}
+                
+
+                
+ 
+                
+                
+            
+        
+    
+
